0363b215547451fb28c755987c45909ecd59e277
Miguel Liezun <liezun.js@gmail.com>
2018-01-27 19:16:48 -0300

    Solved problems with dynamic loading

--- static/index.js
+++ static/index.js
@@ -1,17 +1,37 @@
 $(document).ready(function () {
+    var last_post_id = $('#last-post-id').html()
+    var turns = [true]
     $(window).bind('scroll', function() {
       var scrollTop = $(window).scrollTop()
       var maxScroll = $(document).height() - $(window).height()
-      if (scrollTop/maxScroll > 0.8) {
-        $.get('/more_posts/' + window.last_post_id, function (result) {
-          if (result === '') {
-            $(window).unbind('scroll')
-          } else {  
-            var content = $('#content').html()
-            $('#content').html(content + result)
-          }
-        })
+      var turn = turns.pop()
+      if (turn) {
+        if (scrollTop/maxScroll > 0.8) {
+          $.ajax({
+            url: '/more_posts/' + last_post_id,
+            success: function (result) {
+              if (result === '') {
+                $(window).unbind('scroll')
+              } else {
+                last_post_id = ''
+                for (var i = result.length-1; i >= 0; i--) {
+                  if (result[i] === '#') break
+                  last_post_id = result[i] + last_post_id
+                }
+                result = result.substring(0, i)
+                var posts = $('#posts').html()
+                $('#posts').html(posts + result)
+                turns.push(true)
+              }
+            }
+          })
+        }
+        else {
+          turns.push(true)
+        }
       }
+
    })
+
    $(window).scroll()
 })
